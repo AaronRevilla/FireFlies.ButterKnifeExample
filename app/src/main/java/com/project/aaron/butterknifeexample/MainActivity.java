56fda670f436bb9a64b31334b59d2033905e3bc8
Oscar Aaron Revilla Escalona <aaron_revilla@hotmail.com>
2016-12-12 18:06:23 -0500

    Initial commit

---
+++ app/src/main/java/com/project/aaron/butterknifeexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.project.aaron.butterknifeexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ButterKnifeExmaple";
+    @BindView(R.id.mTextView)
+    TextView textView;
+
+    @BindView(R.id.mEditText)
+    EditText editText;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        textView.setText("Aaron Revilla");
+        editText.setText("Butter Knikfe example");
+
+    }
+
+    @OnClick(R.id.mButton)
+    public void doMagic(){
+        Log.d(TAG, "doMagic: button pressed " + editText.getText());
+    }
+}
